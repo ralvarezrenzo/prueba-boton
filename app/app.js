494af497131fec4adaf0c22f4545b3cdcb218313
renzo <ralvarezrenzo@gmail.com>
2022-03-09 10:29:24 -0500

    intento de menu toggle

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
-app.js
+const d = document;
+
+let boton = d.querySelector('#boton__active');
+let menu = d.querySelector('#menu__active')
+boton.addEventListener('click', openMenu)
+
+function openMenu(e){
+		menu.classList.toggle('open-menu')
+}
