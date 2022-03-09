9304e4a6b727acea842fcb97cfe920d82400c5c3
renzo <ralvarezrenzo@gmail.com>
2022-03-09 16:01:34 -0500

    menu toggle prueba v0.02

--- app/app.js
+++ app/app.js
@@ -7,3 +7,6 @@ boton.addEventListener('click', openMenu)
 function openMenu(e){
 		menu.classList.toggle('open-menu')
 }
+
+// git remote add origin https://github.com/ralvarezrenzo/prueba-boton.git
+// git push -u origin main
